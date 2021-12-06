51d80ed59f5b29243150a155ac37bee684dc3e4b
Nischal Poudel <nschalpdl32@gmail.com>
2021-12-06 03:45:20 -0500

    get route for courses

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "hello"
+    return render_template("index.html")
 
-@app.route('/recommendations')
+
+@app.route('/all-courses', methods=['GET'])
+def all_courses():
+    print("got request")
+    return jsonify(["Course 1", "Course 2", "Course 3"])
+
+
+@ app.route('/recommendations')
 def recommendations():
     # TODO: call solver on template
 
@@ -16,4 +23,3 @@ def recommendations():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
